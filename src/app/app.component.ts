f021b5f0309ea328fad7373afc0c94537ef61164
Nyambura🌻 <bianca@Biancas-MacBook-Air.local>
2022-04-13 15:19:20 +0300

    add quote component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Quote } from './quote'
 
 @Component({
   selector: 'app-root',
@@ -6,11 +7,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-quotes:string[];
+quotes:Quote[] = [
+{id:1, name:'Chop life, problem no dey finish'},
+{id:2, name:'Jispoil, You deserve it'},
+{id:3, name:'Silence is golden, duct tape is silver'},
+{id:4, name:'If you dont survive at first, skydiving is is not for you'},
+];
 
-constructor(){
-this.quotes = [ 'Chop life, problem no dey finish','It is what it is', 'Such is life' ]
-}
+// constructor(){
+// this.quotes = [ 'Chop life, problem no dey finish','It is what it is', 'Such is life' ]
+// }
 
   // quote = 'Chop life, problem no dey finish'
 }
