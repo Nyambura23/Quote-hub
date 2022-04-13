dfc3a7c5f97f015eee4e69bc40a685e07f47cd3c
Nyambura🌻 <bianca@Biancas-MacBook-Air.local>
2022-04-13 14:44:55 +0300

    create quote variable and assign it a value

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  quote = 'Chop life, problem no dey finish';
+  quote = 'Chop life, problem no dey finish'
 }
