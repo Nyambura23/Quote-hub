eb64b8041510da499258bbbac2a180deefd17721
Nyambura🌻 <bianca@Biancas-MacBook-Air.local>
2022-04-14 01:19:30 +0300

    add goal creation date

--- src/app/quote.ts
+++ src/app/quote.ts
@@ -1,6 +1,6 @@
 export class Quote {
 showDescription: boolean;
-constructor(public id: number, public name: string,public description: string){
+constructor(public id: number, public name: string,public description: string, public creationDate: Date){
   this.showDescription=false;
 }
 
