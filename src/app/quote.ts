77b74ca4e9f68c770b3302f1829944e3ff4add0f
Nyambura🌻 <bianca@Biancas-MacBook-Air.local>
2022-04-15 22:20:33 +0300

    finalize styling

--- src/app/quote.ts
+++ src/app/quote.ts
@@ -2,15 +2,9 @@ export class Quote {
 showDescription: boolean;
   upvote: number;
   downvote: number;
-constructor(public id: number, public name: string,public description: string, public likes: number, public dislikes: number, public creationDate: Date){
+constructor(public id: number, public name: string,public description: string,public creator: string, public likes: number, public dislikes: number, public creationDate: Date){
   this.showDescription=false;
-
-  
+ 
 }
-
-  // id: number;
-  // name: string;
-  // description: string;
-
 
 }
--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -9,10 +9,10 @@ import { Quote } from '../quote';
 export class QuoteComponent implements OnInit {
 
   quotes:Quote[] = [
-    new Quote(1, 'Chop life, problem no dey finish', 'The streets',0,0, new Date(2022,1,14),),
-    new Quote(2, 'Jispoil, You deserve it', 'A Kenyan Proverb',0,0, new Date(2022,2,14)),
-    new Quote(3, 'Silence is golden, duct tape is silver','From Case Files',0,0, new Date(2022,3,14)),
-    new Quote(4, 'If you dont survive at first, skydiving is is not for you', 'One Wise man',0,0,new Date(2022,4,14)),
+    new Quote(1, 'Chop life, problem no dey finish', 'The streets','Jaymo',0,0, new Date(2022,1,14),),
+    new Quote(2, 'Jispoil, You deserve it', 'A Kenyan Proverb','Brayo',0,0, new Date(2022,2,14)),
+    new Quote(3, 'Silence is golden, duct tape is silver','From Case Files','Kim',0,0, new Date(2022,3,14)),
+    new Quote(4, 'If you dont survive at first, skydiving is is not for you', 'One Wise man','Kevo',0,0,new Date(2022,4,14)),
     ];
 
     addNewQuote(quote: any){
